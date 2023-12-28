f24d0d7363fcfd2a33d2fc66c9cc99b1e4426315
Andre0221 <103682267+Andre0221@users.noreply.github.com>
2023-12-28 15:58:31 -0300

    Add files via upload

---
+++ aula 88/PRO_1-1_C88_AtividadeDaProfessora2-main/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA6yDcSiDy9k6xFLV_OKuuUSgfKGHMdOQM",
+    authDomain: "aula85-21c84.firebaseapp.com",
+    databaseURL: "https://aula85-21c84-default-rtdb.firebaseio.com",
+    projectId: "aula85-21c84",
+    storageBucket: "aula85-21c84.appspot.com",
+    messagingSenderId: "216472480068",
+    appId: "1:216472480068:web:d8b7e918656fe439684f11"
+  };
